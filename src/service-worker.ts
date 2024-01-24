5807465d087d121f508a168724d94344e79b5079
Freddy Henin <fhenin@managewithstack.com>
2024-01-24 09:39:32 -0500

    I think that works

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,38 +1,71 @@
 
 
- chrome.action.onClicked.addListener(
-  function(activeTab) { 
+let domainsToMatch: string[] = [
+  "youtube.com",
+  "mail.google.com"
+];
+
+let consolidateAllTabsToCurrentWindow = true;
+
+let getTabsToClose = (callbackFn: (tabIds: number[]) => void) => {
+  chrome.tabs.query({}, (tabs) => {
+    let tabIdsToClose: number[] = [];
+    tabs.forEach((tab) => {
+      if (tab.discarded || (isValidUrl(tab.url!) && domainsToMatch.some(d => (new URL(tab.url!).host.indexOf(d) > -1)))) {
+        tabIdsToClose.push(tab.id!);
+      }
+    });
+    callbackFn(tabIdsToClose);
+  });
+}
+
+let isValidUrl = (potentialUrl: string) => {
+  try {
+    new URL(potentialUrl);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+let updateBadgeText = () => {
+  getTabsToClose((tabIds) => {
+    let count = tabIds.length;
+    if (count > 0){
+      chrome.action.setBadgeText({text: count.toString()});
+    }else{
+      chrome.action.setBadgeText({text: ''});
+    }
+  });
+}
+
+chrome.action.onClicked.addListener(
+  function (activeTab) {
     let currentWindowId = activeTab.windowId;
-  
-    chrome.tabs.query({}, function(tabs) { 
-    
-      let tabIds: number[] = [];
 
-      let matchedDomainIds: number[] = [];
+    chrome.tabs.query({}, function (tabs) {
 
-      let domainsToMatch: string[] = [
-        "youtube.com",
-      ];
-      tabs.forEach((tab) => {
-        if (tab.discarded){
-          tabIds.push(tab.id!);
-        }
-        else if (domainsToMatch.some(d => (new URL(tab.url!).host.indexOf(d) > -1)))
-        {
-            matchedDomainIds.push(tab.id!);
-        }
-        else if (tab.windowId !== currentWindowId)
-        {
-          // move to current window
-          chrome.tabs.move(tab.id!, {
-            index: -1,
-            windowId: currentWindowId
-          });
-        }
+      getTabsToClose((tabIds) => {
+        chrome.tabs.remove(tabIds, () => {
+          updateBadgeText();
+        });
       });
 
-      chrome.tabs.remove(tabIds);
-      chrome.tabs.remove(matchedDomainIds);
-     } );
-   },
+      if (consolidateAllTabsToCurrentWindow){
+        tabs.forEach((tab) => {
+          if (tab.windowId !== currentWindowId) {
+            // move to current window
+            chrome.tabs.move(tab.id!, {
+              index: -1,
+              windowId: currentWindowId
+            });
+          }
+        });
+      }
+    });
+  },
 );
+
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
+  updateBadgeText();
+});
